66848f7c2ce9e2d806c347cc6e3b0db89ee9c1dd
ladudasha <darya.gameza@gmail.com>
2022-02-12 15:47:33 +0300

    scroll

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,23 @@
 console.log(test(5, 2));
 console.log(test(2, 4));
 console.log(test(9, 1));
+
+
+// плавный скролл
+
+
+
+  let anchors = document.querySelectorAll("a[href*='#']")
+  console.log(anchors);
+  for(let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+      event.preventDefault()
+      
+      let blockID  = anchor.getAttribute('href');
+      document.querySelector(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+    })
+  }
+
